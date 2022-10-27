0b7436486053596f839c8803d24a0da0934bd52b
Tomasz Bieńkowski <it.bienkowski@gmail.com>
2022-10-27 14:20:41 +0200

    Database docker compose & connection setup

--- src/main/java/com/yobombel/homesuppliesmonitor/Model/Item.java
+++ src/main/java/com/yobombel/homesuppliesmonitor/Model/Item.java
@@ -2,10 +2,20 @@ package com.yobombel.homesuppliesmonitor.Model;
 
 import com.yobombel.homesuppliesmonitor.Model.Enums.Amount;
 import com.yobombel.homesuppliesmonitor.Model.Enums.Category;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.util.UUID;
 
 @Data
+@Entity
 public class Item {
+
+    @Id
+    private Long id;
 
     private String name;
     private Category category;
