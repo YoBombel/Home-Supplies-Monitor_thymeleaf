a491141dd7f90dcf20aba4370ba983dd738ee77d
Tomasz Bieńkowski <it.bienkowski@gmail.com>
2022-10-27 09:33:07 +0200

    Initial commit

---
+++ src/main/java/com/yobombel/homesuppliesmonitor/HomeSuppliesMonitorApplication.java
@@ -0,0 +1,13 @@
+package com.yobombel.homesuppliesmonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeSuppliesMonitorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomeSuppliesMonitorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yobombel/homesuppliesmonitor/HomeSuppliesMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yobombel.homesuppliesmonitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeSuppliesMonitorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
