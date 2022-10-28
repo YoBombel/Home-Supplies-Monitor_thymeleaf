36169c61b79033a6ee747fd1e4ebd044afdc1330
Tomasz Bieńkowski <it.bienkowski@gmail.com>
2022-10-28 10:53:27 +0200

    Basic CRUD added to ItemService

--- src/main/java/com/yobombel/homesuppliesmonitor/Service/ItemService.java
+++ src/main/java/com/yobombel/homesuppliesmonitor/Service/ItemService.java
@@ -2,11 +2,11 @@ package com.yobombel.homesuppliesmonitor.Service;
 
 import com.yobombel.homesuppliesmonitor.Model.Item;
 import com.yobombel.homesuppliesmonitor.Repository.ItemRepository;
-import com.yobombel.homesuppliesmonitor.util.FileParser;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @AllArgsConstructor
@@ -14,8 +14,20 @@ public class ItemService {
 
     private final ItemRepository itemRepository;
 
+    public void saveItem(Item item){
+        itemRepository.save(item);
+    }
+
     public void saveItemList(List<Item> itemList){
         itemRepository.saveAll(itemList);
+    }
+
+    public Optional<Item> findById(Long id){
+        return itemRepository.findById(id);
+    }
+
+    public void deleteById(Long id){
+        itemRepository.deleteById(id);
     }
 
     public List<Item> findAll(){
