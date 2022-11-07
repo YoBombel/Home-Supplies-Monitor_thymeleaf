012cfc47c3c92b27c8004e6c97c7d3ea53d02d0a
Tomasz Bieńkowski <it.bienkowski@gmail.com>
2022-11-07 11:42:50 +0100

    Add new user logic added

--- src/main/java/com/yobombel/homesuppliesmonitor/HomeSuppliesMonitorApplication.java
+++ src/main/java/com/yobombel/homesuppliesmonitor/HomeSuppliesMonitorApplication.java
@@ -3,11 +3,14 @@ package com.yobombel.homesuppliesmonitor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+
 @SpringBootApplication
 public class HomeSuppliesMonitorApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(HomeSuppliesMonitorApplication.class, args);
     }
+
 
 }
--- src/main/java/com/yobombel/homesuppliesmonitor/controller/UserController.java
+++ src/main/java/com/yobombel/homesuppliesmonitor/controller/UserController.java
@@ -4,19 +4,25 @@ import com.yobombel.homesuppliesmonitor.model.User;
 import com.yobombel.homesuppliesmonitor.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequiredArgsConstructor
-public class MainController {
+public class UserController {
 
     private final UserService userService;
 
-    @GetMapping("")
-    public String startPage(Model model){
-        model.addAttribute("users", userService.findAllUsers());
-        return "start";
+    @GetMapping("userpage")
+    public String userPage(){
+        return "userPage";
+    }
+
+    @PostMapping("newUser")
+    public String newUser(@RequestParam String name){
+        userService.createNewUser(new User(name));
+        return "userPage";
     }
 
 }
