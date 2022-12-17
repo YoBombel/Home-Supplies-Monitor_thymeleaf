dda1fefffc53bfdd9237ee15ddb889f30f9a18ff
Tomasz Bieńkowski <it.bienkowski@gmail.com>
2022-12-17 10:20:05 +0100

    Basic loggers added

--- src/main/java/com/yobombel/homesuppliesmonitor/controller/MainController.java
+++ src/main/java/com/yobombel/homesuppliesmonitor/controller/MainController.java
@@ -4,6 +4,8 @@ import com.yobombel.homesuppliesmonitor.model.Supply;
 import com.yobombel.homesuppliesmonitor.model.enums.Amount;
 import com.yobombel.homesuppliesmonitor.service.SupplyService;
 import lombok.RequiredArgsConstructor;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,34 +18,40 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class MainController {
 
     private final SupplyService supplyService;
-
+    private static final Logger LOGGER = LogManager.getLogger(MainController.class);
 
     @GetMapping("/")
     public String allItems(Model model) {
         model.addAttribute("allItemList", supplyService.findAll());
+        LOGGER.info("Received request for main page.");
         return "index";
     }
 
     @PostMapping("/")
     public String addItem(@ModelAttribute Supply newSupply, Model model) {
+        LOGGER.info("User tries to add new item.");
         supplyService.saveItem(newSupply);
+        LOGGER.info("User added new item: " + newSupply);
         return "redirect:/";
     }
 
     @PostMapping("/updateAmount")
     public String changeAmount(@RequestParam String name, @RequestParam String amount, Model model) {
         supplyService.updateAmount(name, Amount.valueOf(amount.toUpperCase()));
+        LOGGER.info("User updated amount of " + name + " to: " + amount);
         return "redirect:/";
     }
 
     @PostMapping("/delete")
     public String delete(@RequestParam String name) {
         supplyService.deleteByName(name);
+        LOGGER.info("User deleted item " + name);
         return "redirect:/";
     }
 
     @GetMapping("/shopping_list")
-    public String shoppingList(Model model){
+    public String shoppingList(Model model) {
+        LOGGER.info("Received request for shopping list.");
         model.addAttribute("shoppingList", supplyService.getLowSupplies());
         return "shopping_list";
     }
