c4be57b01ce303a02280ee4b2d788cf5c4ba549b
Tomasz Bieńkowski <it.bienkowski@gmail.com>
2022-12-19 20:37:03 +0100

    Item limit increased to 100; activated profile prod

--- src/main/java/com/yobombel/homesuppliesmonitor/config/LimitConfig.java
+++ src/main/java/com/yobombel/homesuppliesmonitor/config/LimitConfig.java
@@ -5,6 +5,6 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class LimitConfig {
 
-    public static final int ITEM_LIMIT = 25;
+    public static final int ITEM_LIMIT = 100;
 
 }
